4ee16e90a579800f4c5c8129c8822a96d9d0d8d5
phellweg <peter.hellwege@vizientinc.com>
2022-04-25 10:00:01 -0600

    still trying to get display to hide

--- src/components/RPSResult.js
+++ src/components/RPSResult.js
@@ -6,46 +6,47 @@ import Scissors from '../static/icon-scissors.svg'
 export default () => {
     return (
         <>
-        <div class='resultsWrap'>
+        <div className='resultsWrap'>
             <div>
                 <h2>You Picked</h2>
-                    <div id='paper'>
-                        <div class='hand'>
-                            <img src={Paper} />
-                        </div>
+                <div id='paper'>
+                    <div class='hand'>
+                        <img src={Paper} />
                     </div>
-                    <div id='scissor'>
-                        <div class='hand'>
-                            <img src={Scissors}/>
-                        </div>
+                </div>
+                {/* <div id='scissor'>
+                    <div class='hand'>
+                        <img src={Scissors}/>
                     </div>
-                    <div id="rock">
-                        <div class='hand'>
-                            <img src={Rock} />
-                        </div>
+                </div>
+                <div id="rock">
+                    <div class='hand'>
+                        <img src={Rock} />
                     </div>
+                </div> */}
             </div>
+
             <div id='winLoseDiv'>
                 <h1>You</h1> 
                 <br/>
                 <button id='playAgainButton'>Play Again</button> </div>
             <div>
                 <h2>House Picked</h2>
-                    <div id='paper'>
-                        <div class='hand'>
-                            <img src={Paper} />
-                        </div>
+                <div id='paper'>
+                    <div class='hand'>
+                        <img src={Paper} />
                     </div>
-                    <div id='scissor'>
-                        <div class='hand'>
-                            <img src={Scissors}/>
-                        </div>
+                </div>
+                {/* <div id='scissor'>
+                    <div class='hand'>
+                        <img src={Scissors}/>
                     </div>
-                    <div id="rock">
-                        <div class='hand'>
-                            <img src={Rock} />
-                        </div>
+                </div>
+                <div id="rock">
+                    <div class='hand'>
+                        <img src={Rock} />
                     </div>
+                </div> */}
             </div>
         </div>                
         </>
--- src/components/RPSSelect.js
+++ src/components/RPSSelect.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import '../RPS.css'
-import Rock from '../static/icon-rock.svg'
-import Paper from '../static/icon-paper.svg'
-import Scissors from '../static/icon-scissors.svg'
+import Rock from '../static/icon-rock.svg';
+import Paper from '../static/icon-paper.svg';
+import Scissors from '../static/icon-scissors.svg';
 
+    function Transitioning() {
+        const x = document.getElementsByClassName('selectBkg')
+        const y = document.getElementsByClassName('resultsWrap')
+        if(x.style.display === 'block') {
+            x.style.display = 'none';
+            y.style.display = 'block';
+        }
+        else {
+            y.style.display = 'none';
+            x.style.display = 'block';
+        }
+    }
 export default () => {
+
     return (
         <>
-        <div class='selectBkg'>
+        <div className='selectBkg'>
             <div class='selectWrap'>
-                <div id='paper'>
+                <div id='paper' onClick={Transitioning}>
                     <div class='hand'>
                         <img src={Paper} />
                     </div>
