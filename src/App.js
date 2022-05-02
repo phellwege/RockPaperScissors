f38137b86cb58012cf45b3a439b1466ef00750b4
phellweg <peter.hellwege@vizientinc.com>
2022-05-02 09:45:53 -0600

    attempt 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+// import { BrowserRouter } from 'react-router-dom/browserRouter';
 import { Router } from '@reach/router';
 import Home from './views/Home';
 import './App.css';
