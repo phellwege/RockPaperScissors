0d16a100078f7ad28f5f6eb017627b5a37ad23f1
phellweg <peter.hellwege@vizientinc.com>
2022-05-03 10:47:57 -0600

    mobile version

--- src/components/RPSResult.js
+++ src/components/RPSResult.js
@@ -9,6 +9,29 @@ export default (props) => {
     return (
         <>
         <div className='resultsWrap'>
+                <div className='playersChoice'>
+                    <h2>You Picked</h2>
+                    <div id={props.myChoice} data-id={props.myChoice}>
+                        <div className='hand'>
+                            <img src={props.myChoice === 'rock' ? rock :props.myChoice === 'scissor' ? scissor : props.myChoice === 'paper' ? paper : rock} alt={props.myChoice}/>
+                        </div>
+                    </div>
+                </div>
+                <div id='winLoseDiv'>
+                    {props.PlayerWin=='Win' && <h1>You Win</h1>}
+                    {props.PlayerWin=='Lose' && <h1>You lose</h1>}
+                    {props.PlayerWin=='Draw' && <h1>Draw</h1>}
+                    <br/>
+                    <button id='playAgainButton' onClick={(e)=> {props.Transitioning(); props.setMyChoice('')}}>Play Again</button> 
+                </div>
+                <div className='pcChoice'>
+                    <h2>House Picked</h2>
+                    <div id={props.house} data-id={props.house}>
+                        <div className='hand'>
+                            <img src={props.house === 'rock' ? rock :props.house === 'scissor' ? scissor : props.house === 'paper' ? paper : rock} alt={props.house}/>
+                        </div>
+                    </div>
+                </div>
             <div id='mobileOnly'>
                 <div className='playersChoice'>
                     <h2>You Picked</h2>
